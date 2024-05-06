e80220ae972582fb9374dbbd0537ca06aa453990
Riya Samantaray <110122876+Riyasamantaray23@users.noreply.github.com>
2024-05-06 18:36:55 +0530

    Update crop-webapp.py

--- crop-webapp.py
+++ crop-webapp.py
@@ -17,7 +17,7 @@ def main():
     st.image(image)
     html_temp = """
     <div style="background-color:#85c532; padding:7px">
-    <h2 style="color:#d59944;text-align:center;">Find the most suitable crop using GraMitra's crop recommender.</h2>
+    <h2 style="color:#fff;text-align:center;">Find the most suitable crop using GraMitra's crop recommender.</h2>
     </div>
     """
 
@@ -30,32 +30,4 @@ def main():
     option = st.sidebar.selectbox("Which model would you like to use?", activities)
     st.subheader(option)
     sn = st.slider('NITROGEN (N) in (kg/ha)', 0.0, 150.0)
-    sp = st.slider('PHOSPHOROUS (P) in (kg/ha)', 0.0, 150.0)
-    pk = st.slider('POTASSIUM (K) in (kg/ha)', 0.0, 210.0)
-    pt = st.slider('TEMPERATURE in °C', 0.0, 50.0)
-    phu = st.slider('HUMIDITY in %', 0.0, 100.0)
-    pPh = st.slider('pH', 0.0, 14.0)
-    pr = st.slider('RAINFALL in mm', 0.0, 300.0)
-    inputs = [[sn, sp, pk, pt, phu, pPh, pr]]
-    if st.button('Classify'):
-        if option == 'Logistic Regression':
-            st.success(classify(LogReg_model.predict(inputs)))
-        elif option == 'Decision Tree':
-            st.success(classify(DecisionTree_model.predict(inputs)))
-        elif option == 'Naive Bayes':
-            st.success(classify(NaiveBayes_model.predict(inputs)))
-        else:
-            st.success(classify(RF_model.predict(inputs)))   
-
-if __name__ == '__main__':
-    main()
-
-
-    
- 
-
-
-    
-
-
-
+    sp = st.slider('PHOSPHOROUS (P) in (kg/ha)',

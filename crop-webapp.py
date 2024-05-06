a0ea53e12dd4c28ba6b6c5643cf3242f0e4edb0b
Riya Samantaray <110122876+Riyasamantaray23@users.noreply.github.com>
2024-05-06 18:24:41 +0530

    Update crop-webapp.py

--- crop-webapp.py
+++ crop-webapp.py
@@ -2,18 +2,17 @@ import streamlit as st
 import pickle
 from PIL import Image
 
-LogReg_model=pickle.load(open('LogReg_model.pkl','rb'))
-DecisionTree_model=pickle.load(open('DecisionTree_model.pkl','rb'))
-NaiveBayes_model=pickle.load(open('NaiveBayes_model.pkl','rb'))
-RF_model=pickle.load(open('RF_model.pkl','rb'))
+LogReg_model = pickle.load(open('LogReg_model.pkl', 'rb'))
+DecisionTree_model = pickle.load(open('DecisionTree_model.pkl', 'rb'))
+NaiveBayes_model = pickle.load(open('NaiveBayes_model.pkl', 'rb'))
+RF_model = pickle.load(open('RF_model.pkl', 'rb'))
 
 def classify(answer):
-    return answer[0]+" is the best crop for cultivation here."
+    return "<span style='font-size:24px; font-weight:bold;'>" + answer[0].upper() + "</span> <span style='font-size:18px;'>is the best crop for cultivation here.</span>"
 
 def main():
     st.title("GraMitra (Crop Recommender)...")
-    st.sidebar.image('logo.png', use_column_width=True)  # Add this line to display logo in the sidebar
-    image=Image.open('cc.jpg')
+    image = Image.open('cc.jpg')
     st.image(image)
     html_temp = """
     <div style="background-color:#85c532; padding:7px">
@@ -23,29 +22,34 @@ def main():
 
     st.markdown(html_temp, unsafe_allow_html=True)
     
-   
-
-    activities=['Naive Bayes','Logistic Regression','Decision Tree','Random Forest']
-    option=st.sidebar.selectbox("Which model would you like to use?",activities)
+    # Add logo to the top left corner
+    st.sidebar.image('logo.png', width=150, caption='')
+    
+    activities = ['Naive Bayes', 'Logistic Regression', 'Decision Tree', 'Random Forest']
+    option = st.sidebar.selectbox("Which model would you like to use?", activities)
     st.subheader(option)
-    sn=st.slider('NITROGEN (N) in (kg/ha)', 0.0, 150.0)
-    sp=st.slider('PHOSPHOROUS (P) in (kg/ha)', 0.0, 150.0)
-    pk=st.slider('POTASSIUM (K)in (kg/ha)', 0.0, 210.0)
-    pt=st.slider('TEMPERATURE in °C', 0.0, 50.0)
-    phu=st.slider('HUMIDITY in %', 0.0, 100.0)
-    pPh=st.slider('ph', 0.0, 14.0)
-    pr=st.slider('RAINFALL in mm', 0.0, 300.0)
-    inputs=[[sn,sp,pk,pt,phu,pPh,pr]]
+    sn = st.slider('NITROGEN (N) in (kg/ha)', 0.0, 150.0)
+    sp = st.slider('PHOSPHOROUS (P) in (kg/ha)', 0.0, 150.0)
+    pk = st.slider('POTASSIUM (K) in (kg/ha)', 0.0, 210.0)
+    pt = st.slider('TEMPERATURE in °C', 0.0, 50.0)
+    phu = st.slider('HUMIDITY in %', 0.0, 100.0)
+    pPh = st.slider('pH', 0.0, 14.0)
+    pr = st.slider('RAINFALL in mm', 0.0, 300.0)
+    inputs = [[sn, sp, pk, pt, phu, pPh, pr]]
     if st.button('Classify'):
-        if option=='Logistic Regression':
+        if option == 'Logistic Regression':
             st.success(classify(LogReg_model.predict(inputs)))
-        elif option=='Decision Tree':
+        elif option == 'Decision Tree':
             st.success(classify(DecisionTree_model.predict(inputs)))
-        elif option=='Naive Bayes':
+        elif option == 'Naive Bayes':
             st.success(classify(NaiveBayes_model.predict(inputs)))
         else:
             st.success(classify(RF_model.predict(inputs)))   
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
+
+    
+
+
 
